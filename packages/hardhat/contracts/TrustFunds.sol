2c5e87f12c95fbb7eee0c194f9dd168f54b81985
Gage Bachik <gbachik@gmail.com>
2021-06-22 14:47:02 -0700

    contract v2 without events setup

---
+++ packages/hardhat/contracts/TrustFunds.sol
@@ -0,0 +1,90 @@
+//SPDX-License-Identifier: BUSL-1.1
+
+pragma solidity >=0.8.0 <0.9.0;
+
+import "hardhat/console.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "@openzeppelin/contracts/utils/Address.sol";
+import "@openzeppelin/contracts/utils/structs/EnumerableMap.sol";
+
+contract TrustFundv2 {
+    using Address for address payable;
+    using SafeMath for uint256;
+    using EnumerableMap for EnumerableMap.UintToAddressMap;
+    
+    EnumerableMap.UintToAddressMap private activeBeneficiaries;
+    EnumerableMap.UintToAddressMap private activeTokens;
+    mapping(address => Trust) private trustFunds;
+    mapping(address => uint256) balances;
+    uint256 public totalTrusts;
+    uint256 public totalTokens;
+    
+    struct Trust {
+        bool active;
+        uint256 key;
+        uint256 timelock;
+        mapping(address => uint256) balances;
+    }
+    
+    function addToken(address _token) public{
+        totalTokens = totalTokens.add(1);
+        activeTokens.set(totalTokens, _token);
+    }    
+    
+    function removeToken(address _token) public{
+        for (uint256 i = 0; i < activeTokens.length(); i++) {
+            address currToken = activeTokens.get(i);
+            if (currToken == _token) {
+                activeTokens.remove(i);
+                totalTokens = totalTokens.sub(1);
+            }
+        }
+    }
+    
+    function getBankBalance(address _token) public view returns(uint256){
+        return balances[_token];
+    }    
+    
+    function getTrustBalance(address _token, address _beneficiary) public view returns(uint256){
+        Trust storage currentTrust = trustFunds[_beneficiary];
+        return currentTrust.balances[_token];
+    }
+    
+    function deposit(address _token, address _beneficiary) public payable{
+        Trust storage t = trustFunds[_beneficiary];
+        t.balances[_token] = t.balances[_token].add(msg.value);
+        balances[_token] = balances[_token].add(msg.value);
+    }
+    
+    function createTrust(address _beneficiary, uint256 _timelock) public{
+        Trust storage t = trustFunds[_beneficiary];
+        t.active = true;
+        t.timelock = _timelock;
+        totalTrusts = totalTrusts.add(1);
+        t.key = totalTrusts;
+        activeBeneficiaries.set(totalTrusts, _beneficiary);
+    }
+    
+    function getActiveTrust(address _beneficiary) view public returns(address, uint256, string memory) {
+        Trust storage t = trustFunds[_beneficiary];
+        string memory allBalances;
+        for (uint256 i = 0; i < activeTokens.length(); i++) {
+            address currToken = activeTokens.get(i);
+            abi.encodePacked(allBalances, t.balances[currToken]);
+        }
+        return (_beneficiary, t.timelock, allBalances);
+    }
+    
+    function withdrawTrust(address _beneficiary) public{
+        require(_beneficiary == msg.sender, "You're not the beneficiay of this trust");
+        Trust storage t = trustFunds[_beneficiary];
+        require(block.timestamp > t.timelock, "Trust not mature yet.");
+        require(t.active == true, "Trust already payed out.");
+        address payable payee = payable(msg.sender);
+        t.active = false;
+        for (uint256 i = 0; i < activeTokens.length(); i++) {
+            address currToken = activeTokens.get(i);
+            payee.sendValue(t.balances[currToken]);
+        }
+    }
+}
