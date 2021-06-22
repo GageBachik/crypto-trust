cddd21fd043b078ab2aed7879d07ff997566b2a4
Gage Bachik <gbachik@gmail.com>
2021-06-21 23:12:29 -0700

    fixed yarn deployments

--- packages/hardhat/contracts/YourContract.sol
+++ packages/hardhat/contracts/YourContract.sol
@@ -1,5 +1,5 @@
 pragma solidity >=0.6.0 <0.9.0;
-//SPDX-License-Identifier: BUSL-1.1
+//SPDX-License-Identifier: MIT
 
 import "hardhat/console.sol";
 //import "@openzeppelin/contracts/access/Ownable.sol"; //https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol
